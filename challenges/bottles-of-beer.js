4ec56147b2aa466b568ee1adc4d4f4f3905abccb
nathanallen <thenathanator@gmail.com>
2016-06-27 14:49:05 -0700

    add first challenge, more instructions

---
+++ challenges/bottles-of-beer.js
@@ -0,0 +1,24 @@
+/*
+
+  Create the "Bottles of beer on the wall" song (watch out for infinite loops!):
+
+  ```
+      5 bottles of beer on the wall,
+      5 bottles of beer!
+      Take one down and pass it around,
+      4 bottles of beer on the wall...
+
+      4 bottles of beer on the wall,
+      4 bottles of beer!
+      Take one down and pass it around,
+      3 bottles of beer on the wall...
+      etc.
+  ```
+
+  Bonuses
+  - How would you change "0" to "No more"?
+  - How would you fix "1 bottles of beer"?
+
+*/
+
+// YOUR CODE HERE
