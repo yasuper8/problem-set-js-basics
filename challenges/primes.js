ff37f4736ddb30b0dbe64e672a89539f08fca0fa
melicarls <melissacarlson15@gmail.com>
2016-07-06 09:58:40 -0700

    solution for primes

--- challenges/primes.js
+++ challenges/primes.js
@@ -10,3 +10,22 @@
 */
 
 // YOUR CODE HERE
+
+function isPrime(num) {
+  for (var i=2; i<num; i++) {
+    if (num % i === 0) {
+      // If num is evenly divisble by any number, it is not Prime
+      return false;
+    }
+  }
+  return true;
+}
+
+function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
